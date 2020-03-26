be4140dae20df54f34de6316add88033af1025a2
GabrielNR <gabrieldonascimento103@gmail.com>
2020-03-26 14:32:18 -0300

    Back-end da aplicacao

---
+++ backend/src/notion.js
@@ -0,0 +1,30 @@
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Altera uma informacao no back-end
+ * DELETE: Deletar uma informacao no back-end
+ */
+
+/*
+* Tipos de parametros:
+*
+* Query Params: Parametros nomeados enviados na rota apos o "? (Filtros, Paginação)
+* Route Params: Parametro utilizados para identificar recursos
+* Request Body: Corpo da requisicao, utilizado para criar ou alterar a requisicao
+*/
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CounchDB, etc
+ */
+
+/*
+ * Driver: Select * From users
+ * Query Builder: table('users').select('*').where()
+ */
